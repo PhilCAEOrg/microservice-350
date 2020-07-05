b375d407137c97f4fb6332dc49897a0364161968
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-07-05 09:14:36 +0000

    Micro Init

---
+++ db/350_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  commedit
+-- Automatically generated sql script for the service 350, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tab1.
+--
+CREATE TABLE commedit.tab1 (
+  col1 INTEGER ,
+CONSTRAINT col1_PK PRIMARY KEY (col1)
+ 
+);
+
+
+
